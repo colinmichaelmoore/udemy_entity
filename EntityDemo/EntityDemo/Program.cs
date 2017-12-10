494527885e0d605b0abece4d1f2be5640204b657
colinmichaelmoore <colinmichaelmoore317@intracitygeeks.org>
2017-12-09 20:42:15 -0500

    Up to beginning of section 4

---
+++ EntityDemo/EntityDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new EntityDemoEntities();
+            var post = new Post()
+            {
+                Body = "This is the Body",
+                DatePublished = DateTime.Now,
+                Title = "Title",
+                PostID = 1
+            };
+
+            
+
+            context.Posts.Add(post);
+            context.SaveChanges();
+
+            
+            
+            
+
+        }
+    }
+}
